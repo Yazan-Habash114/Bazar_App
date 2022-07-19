b116813694153c3272430200426516f001912f07
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2022-07-19 19:10:42 +0300

    Write comments

--- Catalog_Server/routes.py
+++ Catalog_Server/routes.py
@@ -12,7 +12,7 @@ def get_books_related_to_topic(book_topic):
 	return search(book_topic)
 
 
-
+# The queries is only two categories, query-by-item & query-by-subject
 queries = {
 	'query-by-item': {
 		'function': get_info
@@ -26,9 +26,11 @@ queries = {
 # Get query and filter it
 @app.route('/<string:query>/<parameter>', methods=['GET'])
 def queryFromDB(query, parameter):
+	# If query is not in two categories
 	if query not in queries:
 		return 'Invalid query method has been called called', 404
 	
+	# Get the procedure from dictionary then call it
 	return queries[query]['function'](parameter)
 
 
@@ -41,8 +43,9 @@ def update_book(book_id):
 
 	data_json = json.loads(request.data)
 
+	# If there is no quantity key in PUT method => Bad request
 	if data_json.get('quantity') is None:
-		abort(422)
+		abort(400)
 
 	quantity = data_json.get('quantity')
 
