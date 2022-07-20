3444b440f2a85f613445236033643e730fff071c
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2022-07-20 11:30:33 +0300

    Update flask_application.py

--- Frontend_Server/flask_application.py
+++ Frontend_Server/flask_application.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-CATALOG_SERVER_IP = "127.0.0.1"
-ORDER_SERVER_IP = "192.168.1.22"
+CATALOG_SERVER_IP = "172.16.102.196"
+ORDER_SERVER_IP = "10.0.2.15"
 CATALOG_PORT = 5000 
 ORDER_PORT = 5001
